fa34acf1fe9de36690c66616140dc6c500a3a09f
riya kondo <riya@riyanoMacBook-Air.local>
2019-02-28 17:11:43 +0900

    setup.sh is modified. NG_LIST is added. shebang is changed to "bin/zsh".

--- setup.sh
+++ setup.sh
@@ -1,14 +1,19 @@
-#! /bin/bash
+#! /bin/zsh
 
 DOT_FILES=( .??* )
+NG_LIST=( .git .gitignore .DS_Store )
 
-if [ ! -d {$HOME/zsh} ]; then
+if [ ! -d $HOME/zsh ]; then
   mkdir $HOME/zsh
 fi
 
 for file in ${DOT_FILES[@]}
 do
-  [[ "$f" == ".git" ]] && continue
-  [[ "$f" == ".DS_Store" ]] && continue
-	ln -s $HOME/dotfiles/$file $HOME/$file
+  if [ ! -d $file ]; then;
+    if (( ${NG_LIST[(I)$file]} )); then
+      echo $file 'is in NG_LIST'
+    else
+	    ln -s $HOME/dotfiles/$file $HOME/$file
+    fi
+  fi
 done
